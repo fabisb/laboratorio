30dce7cd3dc51ea1c03436a242a55f31378d7476
Fabian Silva <silvabravofabian@gmail.com>
2024-06-13 16:55:17 -0400

    setInputCedula mejorado

--- front/app/javascript/examen.js
+++ front/app/javascript/examen.js
@@ -386,7 +386,7 @@ const render = async () => {
             <div class="col-3 d-flex justify-content-end align-content-center">
             <svg xmlns="http://www.w3.org/2000/svg" style="cursor:pointer" width="24" height="24" data-bs-dismiss="modal" fill="green" onclick="setInputCedula(${
               e.cedula
-            })" class="bi bi-check-circle" viewBox="0 0 16 16">
+            },'${e.pre_cedula}')" class="bi bi-check-circle" viewBox="0 0 16 16">
               <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
               <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/>
             </svg>
@@ -482,7 +482,8 @@ const render = async () => {
     }
   }
 };
-function setInputCedula(ci) {
+function setInputCedula(ci,pre) {
+  document.getElementsByName('pre_cedula')[0].value = pre
   document.getElementById("ciInput").value = ci;
   document.getElementById('buscarPacienteButton').click();
 }
@@ -519,7 +520,7 @@ function buscarPacienteInput(value) {
           <div class="col-3 d-flex justify-content-end align-content-center">
           <svg xmlns="http://www.w3.org/2000/svg" data-bs-dismiss="modal" style="cursor:pointer" width="24" height="24" fill="green" onclick="setInputCedula(${
             e.cedula
-          })" class="bi bi-check-circle" viewBox="0 0 16 16">
+          },'${e.pre_cedula}')" class="bi bi-check-circle" viewBox="0 0 16 16">
             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
             <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/>
           </svg>
